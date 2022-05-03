ab7436fdff9e92aca0c29ffc6d4c14162f7fe363
AleksandraKosticc <aleksandra.kosticc@gmail.com>
2022-05-03 20:56:05 +0200

    Initial commit

---
+++ Aleksandra_Kostic/src/projekt/FirefoxGoogleLoad.java
@@ -0,0 +1,19 @@
+package projekt;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class FirefoxGoogleLoad {
+	 @Test
+	  public void testGoogleSearch() throws InterruptedException {
+
+	    System.setProperty("webdriver.gecko.driver","./geckodriver.exe");
+	    WebDriver driver = new FirefoxDriver();
+	    driver.get("http://localhost:4200/");
+	    Thread.sleep(5000); 
+	    
+	    driver.quit();
+	  }
+
+}
